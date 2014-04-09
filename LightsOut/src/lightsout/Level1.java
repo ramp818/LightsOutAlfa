856e2410e80f1563a5c532d6fb2a045d8af07e3e
Ruben Pacheco <pachecaso9436@gmail.com>
2014-04-09 17:05:46 -0500

    no hubo cambios importantes

--- LightsOut/src/lightsout/Level1.java
+++ LightsOut/src/lightsout/Level1.java
@@ -7,9 +7,6 @@ import java.awt.Color;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class Level1 extends JFrame implements Runnable, KeyListener
 {
